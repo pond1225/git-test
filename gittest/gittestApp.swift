b3cafc5b2a7912ec8bd9b775a1dcac4a32411b60
pond1225 <pond1225@gmail.com>
2024-02-28 21:12:39 +0900

    git 업로드

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by t2023-m0049 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
